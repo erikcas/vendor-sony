2cad5bd4ed58e5359df85d0c56f527dd9c6368bc
Erik Castricum <erikcas1972@gmail.com>
2015-06-08 20:58:13 +0200

    amami: add modemfirmware

--- amami/amami-partial.mk
+++ amami/amami-partial.mk
@@ -21,4 +21,25 @@ PRODUCT_PACKAGES += \
 	touch_module_id_0x4e \
 	touch_module_id_0x53 \
 	touch_module_id_0x54 \
-	touch_module_id_0x5d
+	touch_module_id_0x5d \
+	modem_b00 \
+	modem_b01 \
+	modem_b02 \
+	modem_b03 \
+	modem_b04 \
+	modem_b05 \
+	modem_b08 \
+	modem_b11 \
+	modem_b13 \
+	modem_b14 \
+	modem_b15 \
+	modem_b16 \
+	modem_b17 \
+	modem_b18 \
+	modem_b19 \
+	modem_b20 \
+	modem_b21 \
+	modem_b22 \
+	modem_b25 \
+	modem_b26 \
+	modem_mdt
